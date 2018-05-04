bf99ca596b3392a098dc7cebe1269cf1bee27124
James Knighton <iamknighton@gmail.com>
2018-05-03 22:57:00 -0700

    Iterate random batches, but is slow.

--- a.py
+++ a.py
@@ -46,12 +46,41 @@ class Loader(object):
         print('Batches: %d train, %d val' %
               (len(self.train_batches), len(self.val_batches)))
 
+        self.num_train_batches = len(self.train_batches)
+        self.num_val_batches = len(self.val_batches)
+        self.num_batches = self.num_train_batches + self.num_val_batches
+
     def get_sample(self, index):
         x = np.zeros(self.sample_shape, 'uint8')
         for i in range(self.frames_per_sample):
             x[i] = self.vid.get_data(index + i)
         return x
 
+    def get_samples(self, indices):
+        x_shape = (len(indices),) + self.sample_shape
+        x = np.zeros(x_shape, 'uint8')
+        for i, index in enumerate(indices):
+            x[i] = self.get_sample(index)
+        return x
+
+    def get_batch(self, is_training, batch_index):
+        if is_training:
+            indices = self.train_batches[batch_index]
+        else:
+            indices = self.val_batches[batch_index]
+        return self.get_samples(indices)
+
+    def each_batch(self):
+        n = self.num_train_batches
+        train_pairs = zip([1] * n, np.arange(n))
+        n = self.num_val_batches
+        val_pairs = zip([0] * n, np.arange(n))
+        pairs = list(train_pairs) + list(val_pairs)
+        np.random.shuffle(pairs)
+        for is_training, batch_index in pairs:
+            yield self.get_batch(is_training, batch_index)
+
 
 loader = Loader('data/train.mp4')
-x = loader.get_sample(1337)
+for x in loader.each_batch():
+    print(x.shape, x.dtype, x.mean(), x.min(), x.max())
