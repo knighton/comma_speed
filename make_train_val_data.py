86fbf7dd09b7865e55f4b86b717618320382245e
James Knighton <iamknighton@gmail.com>
2018-05-04 00:02:29 -0700

    Extract training samples into train/val sets.

---
+++ make_train_val_data.py
@@ -0,0 +1,75 @@
+from argparse import ArgumentParser
+import imageio
+import numpy as np
+from tqdm import tqdm
+
+
+def parse_flags():
+    a = ArgumentParser()
+    a.add_argument('--in', type=str, required=True)
+    a.add_argument('--out_train', type=str, required=True)
+    a.add_argument('--out_val', type=str, required=True)
+    a.add_argument('--frame_count', type=int, default=4)
+    a.add_argument('--frame_skip', type=int, default=8)
+    a.add_argument('--val_frac', type=float, default=0.2)
+    a.add_argument('--keep_frac', type=float, default=0.5)
+    return a.parse_args()
+
+
+def extract_sample(vid, begin_index, sample_shape, frame_count, frame_skip):
+    x = np.zeros(sample_shape, 'uint8')
+    for i in range(frame_count):
+        index = begin_index + i * frame_skip
+        x[i] = vid.get_data(index)
+    return x
+
+
+def process_split(vid, indices, out_filename, sample_shape, frame_count,
+                  frame_skip):
+    x_shape = (len(indices),) + sample_shape
+    x = np.zeros(x_shape, 'uint8')
+    for i, index in enumerate(tqdm(indices)):
+        x[i] = extract_sample(vid, index, sample_shape, frame_count, frame_skip)
+    x.tofile(out_filename)
+
+
+def run(flags):
+    tqdm.monitor_interval = 0
+
+    vid = imageio.get_reader(getattr(flags, 'in'), 'ffmpeg')
+    num_samples = len(vid) - flags.frame_count * flags.frame_skip + 1
+
+    is_sample_train = flags.val_frac < np.random.uniform(0, 1, num_samples)
+
+    train_indices = []
+    val_indices = []
+    for i, is_training in enumerate(is_sample_train):
+        if is_training:
+            train_indices.append(i)
+        else:
+            val_indices.append(i)
+    np.random.shuffle(train_indices)
+    np.random.shuffle(val_indices)
+    print('Sample counts: %d train, %d val' %
+          (len(train_indices), len(val_indices)))
+
+    train_indices = train_indices[:int(len(train_indices) * flags.keep_frac)]
+    val_indices = val_indices[:int(len(val_indices) * flags.keep_frac)]
+    print('Kept sample counts: %d train, %d val' %
+          (len(train_indices), len(val_indices)))
+
+    im = vid.get_data(0)
+    sample_shape = (flags.frame_count,) + im.shape
+    print('Sample shape: %s (%d bytes)' % (sample_shape, np.prod(sample_shape)))
+
+    print('Processing training split...')
+    process_split(vid, train_indices, flags.out_train, sample_shape,
+                  flags.frame_count, flags.frame_skip)
+
+    print('Processing validation split...')
+    process_split(vid, val_indices, flags.out_val, sample_shape,
+                  flags.frame_count, flags.frame_skip)
+
+
+if __name__ == '__main__':
+    run(parse_flags())
---
+++ make_train_val_data.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+python make_train_val_data.py --in data/train_128.mp4 --out_train data/train_128_train.npy --out_val data/train_128_val.npy
