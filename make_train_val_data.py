7274e40d06014951e15dce8b1a11cffdac374547
James Knighton <iamknighton@gmail.com>
2018-05-04 18:12:59 -0700

    Better names.

--- make_train_val_data.py
+++ make_train_val_data.py
@@ -11,27 +11,27 @@ def parse_flags():
     a.add_argument('--out_train_indices', type=str, required=True)
     a.add_argument('--out_val_clips', type=str, required=True)
     a.add_argument('--out_val_indices', type=str, required=True)
-    a.add_argument('--frame_count', type=int, default=4)
-    a.add_argument('--frame_skip', type=int, default=8)
+    a.add_argument('--clip_len', type=int, default=5)
+    a.add_argument('--clip_stride', type=int, default=10)
     a.add_argument('--val_frac', type=float, default=0.2)
     a.add_argument('--keep_frac', type=float, default=0.5)
     return a.parse_args()
 
 
-def extract_sample(vid, begin_index, sample_shape, frame_count, frame_skip):
-    x = np.zeros(sample_shape, 'uint8')
-    for i in range(frame_count):
-        index = begin_index + i * frame_skip
+def extract_clip(vid, begin_index, clip_shape, clip_len, clip_stride):
+    x = np.zeros(clip_shape, 'uint8')
+    for i in range(clip_len):
+        index = begin_index + i * clip_stride
         x[i] = vid.get_data(index)
     return x
 
 
-def process_split(vid, indices, out_frames_fn, out_indices_fn, sample_shape,
-                  frame_count, frame_skip):
-    x_shape = (len(indices),) + sample_shape
+def process_split(vid, indices, out_frames_fn, out_indices_fn, clip_shape,
+                  clip_len, clip_stride):
+    x_shape = (len(indices),) + clip_shape
     x = np.zeros(x_shape, 'uint8')
     for i, index in enumerate(tqdm(indices)):
-        x[i] = extract_sample(vid, index, sample_shape, frame_count, frame_skip)
+        x[i] = extract_clip(vid, index, clip_shape, clip_len, clip_stride)
     x.tofile(out_frames_fn)
 
     indices = np.array(indices, 'int32')
@@ -42,7 +42,7 @@ def run(flags):
     tqdm.monitor_interval = 0
 
     vid = imageio.get_reader(getattr(flags, 'in'), 'ffmpeg')
-    num_samples = len(vid) - flags.frame_count * flags.frame_skip + 1
+    num_samples = len(vid) - flags.clip_len * flags.clip_stride + 1
 
     is_sample_train = flags.val_frac < np.random.uniform(0, 1, num_samples)
 
@@ -64,17 +64,17 @@ def run(flags):
           (len(train_indices), len(val_indices)))
 
     im = vid.get_data(0)
-    sample_shape = (flags.frame_count,) + im.shape
-    print('Sample shape: %s (%d bytes)' % (sample_shape, np.prod(sample_shape)))
+    clip_shape = (flags.clip_len,) + im.shape
+    print('Sample shape: %s (%d bytes)' % (clip_shape, np.prod(clip_shape)))
 
     print('Processing training split...')
     process_split(vid, train_indices, flags.out_train_clips,
-                  flags.out_train_indices, sample_shape, flags.frame_count,
-                  flags.frame_skip)
+                  flags.out_train_indices, clip_shape, flags.clip_len,
+                  flags.clip_stride)
 
     print('Processing validation split...')
     process_split(vid, val_indices, flags.out_val_clips, flags.out_val_indices,
-                  sample_shape, flags.frame_count, flags.frame_skip)
+                  clip_shape, flags.clip_len, flags.clip_stride)
 
 
 if __name__ == '__main__':
