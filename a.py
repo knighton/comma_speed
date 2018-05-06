a195bc465e8a83bf3d9e3f2ba9861645be2cfbb2
James Knighton <iamknighton@gmail.com>
2018-05-06 12:56:43 -0700

    Middle part.

--- a.py
+++ a.py
@@ -7,7 +7,7 @@ def relate(grid, context, relater, global_pool=None):
     # Get shapes.
     batch_size, num_grid_channels = grid.shape[:2]
     spatial_shape = grid.shape[2:]
-    num_cells = np.prod(spatial_shape)
+    num_cells = int(np.prod(spatial_shape))
 
     # Flatten `grid` shapewise and put the channels dimension last.
     cells = grid.view(batch_size, num_grid_channels, num_cells)
@@ -19,7 +19,30 @@ def relate(grid, context, relater, global_pool=None):
     right = cells.unsqueeze(2)
     right = right.repeat(1, 1, num_cells, 1)
 
-    print(cells.shape, left.shape, right.shape)
+    print('cells', cells.shape)
+    print('left', left.shape)
+    print('right', right.shape)
+
+    # Create grid x grid, concatenating context to each if given.
+    if context is None:
+        grid_x_grid = torch.cat([left, right], 0)
+    else:
+        context = context.unsqueeze(1)
+        context = context.repeat(1, num_cells, 1)
+        context = context.unsqueeze(2)
+        context = context.repeat(1, 1, num_cells, 1)
+        print('context', context.shape)
+        grid_x_grid = torch.cat([left, right, context], 3)
+
+    print('grid x grid', grid_x_grid.shape)
+
+    # Reshape for feeding cell pairs to relater.
+    relatee = grid_x_grid.view(batch_size * num_cells * num_cells, -1)
+    print('relatee', relatee.shape)
+
+    # Relate each pair of vectors (with optional context).
+    related = relater(relatee)
+    print('related', related.shape)
 
 
 class Relater(nn.Module):
@@ -29,6 +52,14 @@ class Relater(nn.Module):
 
 grid = torch.rand(32, 64, 4, 4)
 context = None
+relater = Relater()
+global_pool = None
+relate(grid, context, relater, global_pool)
+
+print('-' * 80)
+
+grid = torch.rand(32, 64, 4, 4)
+context = torch.rand(32, 27)
 relater = Relater()
 global_pool = None
 relate(grid, context, relater, global_pool)
