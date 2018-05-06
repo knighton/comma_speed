8df504b959fa805900ba4c4d8c36b3e175c6c69a
James Knighton <iamknighton@gmail.com>
2018-05-06 13:43:35 -0700

    ConvModel.

--- a.py
+++ a.py
@@ -108,11 +108,51 @@ class RelateWith(ptnn.Module):
         self.global_pool = global_pool
 
 
+class ConvModel(ptnn.Module):
+    def __init__(self, n=8):
+        super().__init__()
+
+        # Conv blocks:
+
+        # 128 -> 64.
+        c1 = nn.Conv3d(3, n, 5, 2, 2) + nn.BatchNorm3d(n) + nn.ReLU
+
+        # 64 -> 32.
+        c2 = nn.Conv3d(n, n, 5, 2, 2) + nn.BatchNorm3d(n) + nn.ReLU
+
+        # 32 -> 16.
+        c3 = nn.Conv3d(n, n, 5, 2, 2) + nn.BatchNorm3d(n) + nn.ReLU
+
+        # 16 -> 8.
+        c4 = nn.Conv3d(n, n, 5, 2, 2) + nn.BatchNorm3d(n) + nn.ReLU + \
+            nn.Dropout2d
+
+        # 8 -> 4.
+        c5 = nn.Conv3d(n, n, 5, 2, 2) + nn.BatchNorm3d(n) + nn.ReLU + \
+            nn.Dropout2d
+
+        # Fully-connected blocks:
+        d1 = nn.Linear(4 * 4 * n, n) + nn.BatchNorm1d(n) + nn.Dropout + nn.ReLU
+        d2 = nn.Linear(n, 1) + nn.ReLU
+
+        self.seq = c1 + c2 + c3 + c4 + c5 + nn.Flatten + d1 + d2
+
+    def forward(self, x):
+        return self.seq(x)
+
+
 for global_pool in [None, 'avg', 'max']:
     print('=' * 80)
     print(global_pool)
     print()
 
+    model = ConvModel()
+
+    x = torch.rand(32, 3, 6, 128, 128)
+    y = model(x)
+    print(x.shape, '->', y.shape)
+
+    """
     grid = torch.rand(32, 64, 4, 4)
     context = None
     relater = DefaultRelater(64 * 2, 16)
@@ -126,3 +166,4 @@ for global_pool in [None, 'avg', 'max']:
     relater = DefaultRelater(64 * 2 + 27, 16)
     y = api_relate(grid, context, relater, global_pool)
     print(y.shape)
+    """
